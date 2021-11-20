f2844903c0522c55812299b02cfb68668da326f0
AlinMGupta <guptalin@gmail.com>
2021-11-20 17:44:09 -0500

    added header

--- include/header.php
+++ include/header.php
@@ -5,6 +5,13 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   </head>
   <body>
-    <div class="w3-container w3-round-large w3-amber">
+    <div class="w3-container w3-amber">
       <h1> Hello World </h1>
+    </div>
+    <div class="w3-bar w3-pale-yellow">
+      <a href="#" class="w3-bar-item w3-button">Home</a>
+      <a href="#" class="w3-bar-item w3-button">Learn</a>
+      <a href="#" class="w3-bar-item w3-button">Watch</a>
+      <a href="#" class="w3-bar-item w3-button">Play</a>
+      <a href="#" class="w3-bar-item w3-button">About Us</a>
     </div>
