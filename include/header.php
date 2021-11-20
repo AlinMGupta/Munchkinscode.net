b837de9e0996beb06dbeb7c6d035b863415ebf4c
AlinMGupta <guptalin@gmail.com>
2021-11-20 17:32:28 -0500

    messing around

--- include/header.php
+++ include/header.php
@@ -1,9 +1,10 @@
 <html lang=en style="height: 100%;">
-
-<head>
-  <meta charset=utf-8>
-  <title>Munchkins Code</title>
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-</head>
-
-<body>
+  <head>
+    <meta charset=utf-8>
+    <title>Munchkins Code</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  </head>
+  <body>
+    <div class="w3-container w3-round-large w3-margin w3-amber">
+      <h1> Hello World </h1>
+    </div>
--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php include 'include/header.php';?>
 <?php include 'include/sidenav.php';?>
-<!-- Title -->
-<div class="w3-container w3-round-large w3-margin w3-amber">
-  <h1> Hello World </h1>
-</div>
 <div class="w3-display-container w3-green" style="height:300px;">
   <div class="w3-display-topleft">Top Left</div>
   <div class="w3-display-topright">Top Right</div>
