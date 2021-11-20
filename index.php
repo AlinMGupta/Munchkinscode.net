b8195dd4e2bc47aefdcd157cd2ea2685e9b3718f
AlinMGupta <guptalin@gmail.com>
2021-11-20 11:49:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php include 'inc/header.php';?>
-<?php include 'inc/sidenav.php';?>
+<?php include 'include/header.php';?>
+<?php include 'include/sidenav.php';?>
 <!-- Title -->
 <div class="w3-container w3-amber">
   <h1> Hello World </h1>
 </div>
-<?php include 'inc/footer.php';?>
+<?php include 'include/footer.php';?>
