848584c197081546ec4a73a4e0f87249a9aac4d6
AlinMGupta <guptalin@gmail.com>
2021-11-20 17:30:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php include 'include/header.php';?>
 <?php include 'include/sidenav.php';?>
 <!-- Title -->
-<div class="w3-container w3-round-xlarge w3-amber">
+<div class="w3-container w3-round-large w3-margin w3-amber">
   <h1> Hello World </h1>
+</div>
+<div class="w3-display-container w3-green" style="height:300px;">
+  <div class="w3-display-topleft">Top Left</div>
+  <div class="w3-display-topright">Top Right</div>
+  <div class="w3-display-bottomleft">Bottom Left</div>
+  <div class="w3-display-bottomright">Bottom Right</div>
+  <div class="w3-display-left">Left</div>
+  <div class="w3-display-right">Right</div>
+  <div class="w3-display-middle">Middle</div>
+  <div class="w3-display-topmiddle">Top Mid</div>
+  <div class="w3-display-bottommiddle">Bottom Mid</div>
 </div>
 <?php include 'include/footer.php';?>
