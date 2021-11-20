907c4678fd2316a391ca6b1cbd5469d4c2d467d1
AlinMGupta <guptalin@gmail.com>
2021-11-20 11:27:39 -0500

    created space for website revamp

--- python/1.py
+++
@@ -1,4 +0,0 @@
-Candy = ['Hersheys', 'Candy Corn', 'Sour Patch Kids', 'Skittles', 'Almond Joy', 'Milky Way', 'Kit Kat']
-
-for x in Candy:
-    print(x)
--- python/2.py
+++
@@ -1,9 +0,0 @@
-i = 1
-n = 1
-while n<5:
-    if i == 0:
-        i = 1
-    else:
-        i = 0
-    n += 1
-    print(i)
