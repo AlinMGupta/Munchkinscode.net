7ed741113f4b1b4c0494e93503d1ca69f7523086
AlinMGupta <guptalin@gmail.com>
2021-11-20 11:32:11 -0500

    setup skeleton for new project

--- include/footer.php
+++ include/footer.php
@@ -1,17 +1,3 @@
-<div id="footer" class="footer m-container m-white">
-  <hr />
-  <div class="m-row-padding m-center m-small" style="margin:0 -16px">
-    <div class="m-col l3 m3 s12">
-      <a class="m-button m-light-grey m-block" href="/about/default.asp" target="_top" style="text-decoration:none;margin-top:1px;margin-bottom:1px">ABOUT</a>
-    </div>
-  </div>
-  <hr />
-</div>
-<div class="m-center m-small m-opacity">
-  MunchkinsCode is optimized for learning and training. Examples might be simplified to improve reading and learning.
-  Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content.
-  <a href="//www.w3schools.com/w3css/default.asp">MunchkinsCode is Powered by W3.CSS</a>.<br><br>
-</div>
 </div>
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include 'inc/header.php';?>
+<?php include 'inc/sidenav.php';?>
+<?php include 'inc/footer.php';?>
